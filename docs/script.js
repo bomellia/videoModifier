7aa5a5e45d444529f466357e8faae93f03e06eb6
bomellia <fit4454@gmail.com>
2026-02-18 14:47:42 +0900

    aa

--- docs/script.js
+++ docs/script.js
@@ -25,7 +25,7 @@ function formatTime(seconds) {
   const mins = Math.floor(seconds / 60);
   const secs = Math.floor(seconds % 60);
   const ms = Math.floor((seconds % 1) * 100);
-  return `${mins}:${secs.toString().padStart(2, "0")}:${ms.toString().padStart(2, "0")}`;
+  return `${mins}:${secs.toString().padStart(2, "0")}.${ms.toString().padStart(2, "0")}`;
 }
 
 /* 動画選択 */
